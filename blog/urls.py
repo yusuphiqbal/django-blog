4842d774dca55453ed8ad88c7fc2168a787cdef0
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-04 21:35:47 +0300

    Added url pattern for post_share view

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     # post views
     # path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]

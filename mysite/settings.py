c4b4c4b982236757d782b1d8c4c9340a5957176d
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-04 21:33:23 +0300

    Added email setting for django to send email to console

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Email configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

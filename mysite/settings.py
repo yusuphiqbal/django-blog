36f7a964513e5287c691491d80d5b03a283db331
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-05 23:56:54 +0300

    Updated database settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog',
+        'USER': os.environ.get('DB_USER'),
+        'PORT': os.environ.get('PORT'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': 'localhost'
     }
 }
 

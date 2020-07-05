bf49f2a0096bd2148197fd7e32c42f2598bd59c3
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-05 10:16:19 +0300

    Created a model form for submitting comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
@@ -6,3 +7,14 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'you@example.com'}))
     to = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'friend@example.com'}))
     comments = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your comments'}))
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }

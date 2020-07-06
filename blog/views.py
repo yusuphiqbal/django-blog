bfc97bce3234ffa8ff152e10866950b2a547fbac
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-06 11:38:33 +0300

    Added logic to search posts by trigram similarity

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
@@ -20,7 +20,7 @@ def post_search(request):
             query = form.cleaned_data['query']
             search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
             search_query = SearchQuery(query)
-            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query),).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
